79c4b4f832a1c9d5d888caeaca6e2d614ff2f086
shruthishruu05 <shruthi.m@perfios.com>
2021-08-30 01:16:22 +0530

    REFACTOORED : Printing array using generics

--- src/com/bridgelabz/PrintArray/PrintArray.java
+++ src/com/bridgelabz/PrintArray/PrintArray.java
@@ -1,39 +1,27 @@
 package com.bridgelabz.PrintArray;
 
-public class PrintArray {
-	public static void main(String[] args) {	
-		Integer[] integerArr = {5,6,7};
-		Double[] doubleArr = {6.33 , 3.89, 7.99};
-		Character[] charArr = {'P', 'E', 'R', 'F', 'I', 'O','S'};
-		
-		toPrint(integerArr);
-		toPrint(doubleArr);
-		toPrint(charArr);
+public class PrintArray 
+{
+		public static void main(String[] args) 
+		{		
+			Integer[] integerArr = {5,6,7};
+			Double[] doubleArr = {6.33 , 3.89, 7.99};
+			Character[] charArr = {'P', 'U', 'R', 'F', 'I', 'O','S'};
+			toPrint(integerArr);
+			toPrint(doubleArr);
+			toPrint(charArr);
+			}
+			
+			
+		private static <e> void toPrint(e[] inputArr) 
+		{
+			for(e element : inputArr)
+			{
+				System.out.println(element);
+			}
 		}
-		
-	private static void toPrint(Integer[] a) {
-		
-		for(int element : a) {
-			System.out.println(element);
-		}
-		
-	}
-	
-	private static void toPrint(Double[] b) {
-		
-		for(double element : b) {
-			System.out.println(element);
-		}
-		
-	}
-	
-	private static void toPrint(Character[] c) {
-		
-		for(char element : c) {
-			System.out.println(element);
-		}
-		
-	}
 }
+
+
 
 
